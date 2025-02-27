de2a48d1b4347ece5bf7d47d49900f2f193b502e
nika robakidze <145992705+idk655@users.noreply.github.com>
2025-02-27 21:04:58 +0400

    stickyfilter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
+import { DetailsComponent } from './details/details.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
-    {path: "products", component: ProductsComponent}
+    {path: "products", component: ProductsComponent},
+    {path: "details", component: DetailsComponent}
 ];
