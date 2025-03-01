6f305fd45f736f28c8fcb0c174a6334ae36f7892
nika robakidze <145992705+idk655@users.noreply.github.com>
2025-03-01 18:18:03 +0400

    pages

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -11,31 +11,31 @@ export class ApiService {
     return this.api.get(`https://api.everrest.educata.dev/shop/products/all?page_index=${index}&page_size=${size}`)
   }
 
-  getAllProducts(index: number){
-    return this.api.get(`https://api.everrest.educata.dev/shop/products/all?page_index=${index}&page_size=15`)
-  }
-
-  brands(){
-    return this.api.get(`https://api.everrest.educata.dev/shop/products/brands`)
-  }
-
   filterBrands(brand: string){
     return this.api.get(`https://api.everrest.educata.dev/shop/products/brand/${brand}?page_index=1&page_size=50`)
   }
 
   searchByName(keyword: string){
-    return this.api.get(`https://api.everrest.educata.dev/shop/products/search?page_size=50&keywords=${keyword}`)
+    return this.api.get(`https://api.everrest.educata.dev/shop/products/search?page_size=15&keywords=${keyword}`)
   }
 
   byCategory(category: number){
     return this.api.get(`https://api.everrest.educata.dev/shop/products/search?page_index=1&page_size=50&category_id=${category}`)
   }
 
+  filterall(object: any){
+    return this.api.get(`https://api.everrest.educata.dev/shop/products/search?page_index=1&page_size=50${object.direction}${object.by}${object.min}${object.max}`)
+  }
+
   categories(){
     return this.api.get(`https://api.everrest.educata.dev/shop/products/categories`)
   }
 
-  filterall(object: any){
-    return this.api.get(`https://api.everrest.educata.dev/shop/products/search?page_index=1&page_size=50${object.direction}${object.by}${object.min}${object.max}`)
+  brands(){
+    return this.api.get(`https://api.everrest.educata.dev/shop/products/brands`)
+  }
+
+  getAllProducts(pages: any){
+    return this.api.get(`https://api.everrest.educata.dev/shop/products/all?page_index=${pages}&page_size=10`)
   }
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -12,29 +12,64 @@ import { FilterComponent } from './filter/filter.component';
 })
 export class ProductsComponent {
   constructor(public api: ApiService){
-    this.getCards()
+    this.getCards(this.currentpage)
   }
 
   products: any = []
+  pages:number[] = []
+  pageSize: number = 15
+  currentpage: number = 1
 
+  pagesize(infoget: any){
+    this.products = infoget
 
-  getCards(){
-    this.api.getAllProducts(1).subscribe((data) => this.products = data)
+    let pageQuantity = Math.ceil(infoget.total / infoget.limit) 
+    
+    this.pages = []
+    for(let i = 1; i <= pageQuantity; i++) {
+      this.pages.push(i)
+    }
+    
+  }
+
+  next(){
+    this.getCards(this.currentpage + 1)
+    console.log(this.currentpage)
+  }
+
+  prev(){
+    this.currentpage = this.currentpage - 1
+  }
+
+  getCards(thispage: number){
+    this.api.getAllProducts(thispage).subscribe((data) => {
+      this.products = data
+      this.pagesize(data)
+    })
   }
 
   searchByName(event: string){
-    this.api.searchByName(event).subscribe((data) => this.products = data)
+    this.api.searchByName(event).subscribe((data) => {
+      this.products = data
+      this.pagesize(data)
+    })
+  }
+
+  filterByBrands(brands: string){
+    this.api.filterBrands(brands).subscribe((data) => {
+      this.products = data
+      this.pagesize(data)
+    })
   }
 
   categories(id: number){
     this.api.byCategory(id).subscribe((data) => this.products = data)
   }
 
-  filterByBrands(brands: string){
-    this.api.filterBrands(brands).subscribe((data) => this.products = data)
-  }
-
   directiontransport(obj: any){
-    this.api.filterall(obj).subscribe((data) => this.products = data)
+    this.api.filterall(obj).subscribe((data) => {
+      this.products = data
+      this.pagesize(data)
+    })
   }
 }
